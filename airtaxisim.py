250307446cd63e54f78b8a4b253de2c82f24e487
Mikael Yeghiazaryan <mikaegiazaryan2000@gmail.com>
2025-09-18 12:32:40 +0300

    Rename package to airtaxisim, update version, and remove unused functions

--- airtaxisim.py
+++ airtaxisim.py
@@ -2,22 +2,9 @@
 AirTaxiSim - Python library for AirTaxiSim environment.
 """
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 
 class InvalidKindError(Exception):
     """Raised if the kind is invalid."""
     pass
-
-
-def get_random_ingredients(kind=None):
-    """
-    Return a list of random ingredients as strings.
-
-    :param kind: Optional "kind" of ingredients.
-    :type kind: list[str] or None
-    :raise airtaxisim.InvalidKindError: If the kind is invalid.
-    :return: The ingredients list.
-    :rtype: list[str]
-    """
-    return ["shells", "gorgonzola", "parsley"]
